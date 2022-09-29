d0e894881dca79d19cbcb3253a39c0412f377af9
Benjamin Gutierrez <ben77gome@gmail.com>
2022-09-29 13:29:13 +0000

    Cambios de la primera practica

--- hello.c
+++ hello.c
@@ -2,8 +2,20 @@
 TECNOLOGICO DE MONTERREY CAMPUS GDA
 */
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main(){
-    printf("Hola Mundo\n");
+    int pid = fork();
+
+    if(pid == 0){
+        printf("Soy el proceso hijo\n");
+        excecl("/workspace/TC1104b.514/hello","hello",NULL);
+        sleep(5);
+        pritnf("nunca nunca nunca")
+    }else{
+        printf("Soy el proceso padre\n");
+        wait(NULL);
+    }
     return 0;
 }
