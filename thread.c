0bbdefdc0e492117d4e877c235fc9065eb565362
Benjamin Gutierrez <ben77gome@gmail.com>
2022-10-13 13:48:15 +0000

    Guardado

---
+++ thread.c
@@ -0,0 +1,31 @@
+#include <pthread.h>
+#include <stdio.h>
+
+typedef struct thread_data{
+    int threadId;
+    char name[20];
+} ThreadData;
+typedef char caracter;
+
+void * helloWorld(void *arg){
+    ThreadData *myData = (ThreadData *)arg;
+    printf("Hola desde el hilo y mi id es % d\n", myData -> threadId);
+    pthread_exit(NULL);
+   //return NULL;
+}
+
+
+int main(){
+    for(int i = 0; i < 100; i++){
+        ThreadData myData;
+        pthread_t threadId;
+        myData.threadId = i;
+        pthread_create(&threadId,NULL,helloWorld,(void *) &myData);
+    }
+    pthread_exit(NULL); 
+    printf("Nunca llega");
+    //return 0;
+}
+
+//gcc -o thread thread.c -lpthread
+//./thread
